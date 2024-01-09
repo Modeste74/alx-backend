53a41c92e156b3376b5ef8ee4050ce9bc9cc290c
Modeste Ciira <ciiramodeste23@gmail.com>
2024-01-09 19:16:34 +0300

    Get locale from request

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -5,6 +5,7 @@ from flask_babel import Babel
 
 
 class Config(object):
+    """configuration for langauges"""
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
