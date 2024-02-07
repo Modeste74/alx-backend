f56a0bbcfaad265a849c6db9e65365407b7b1b75
Modeste74 <ciiramodeste23@gmail.com>
2024-02-06 22:43:19 -1200

    create the job creator

--- 0x03-queuing_system_in_js/6-job_creator.js
+++ 0x03-queuing_system_in_js/6-job_creator.js
@@ -2,24 +2,21 @@ const kue  = require('kue');
 const queue = kue.createQueue();
 
 const jobData = {
-  phoneNumber: '1234567890',
-  message: 'Hello, this is a notification!'
+  phoneNumber: "1234567890",
+  message: "Hello, this is a notification!"
 };
 
 const notificationJob = queue.create('push_notification_code', jobData);
 
-notificationJob
-  .on('complete', () => {
-    console.log('Notification job completed');
-    process.exit(0);
-  })
-  .on('failed', () => {
-    console.log('Notification job failed');
-    process.exit(1);
-  });
+notificationJob.on('complete', () => {
+  console.log('Notification job completed');
+});
+notificationJob.on('failed', () => {
+  console.log('Notification job failed');
+});
 
 notificationJob.save((err) => {
-    if (!err) {
-        console.log(`Notification job created: ${notificationJob.id}`);
-    }
+  if (!err) {
+    console.log(`Notification job created: ${notificationJob.id}`);
+  }
 });
